dcee238fb768eb45b99473f99ec196d011336b6d
SyntaxLexx <spencermainaa@gmail.com>
2023-04-16 01:29:58 +0300

    feat: added missing user relationships

--- app/Models/UserRelationshipsTrait.php
+++ app/Models/UserRelationshipsTrait.php
@@ -2,7 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 trait UserRelationshipsTrait
 {
+    public function logins(): HasMany
+    {
+        return $this->hasMany(UserLogin::class);
+    }
 
+    public function contactMessages(): HasMany
+    {
+        return $this->hasMany(ContactMessage::class);
+    }
 }

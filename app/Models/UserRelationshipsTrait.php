e5c5b430220e9baf3218c4e8c742295a5bacdced
SyntaxLexx <spencermainaa@gmail.com>
2023-05-17 22:29:28 +0300

    feat: added latestLogin rlship. Returns a HasOne

--- app/Models/UserRelationshipsTrait.php
+++ app/Models/UserRelationshipsTrait.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 trait UserRelationshipsTrait
 {
     public function logins(): HasMany
     {
         return $this->hasMany(UserLogin::class);
+    }
+
+    public function latestLogin(): HasOne
+    {
+        return $this->logins()->one()->latestOfMany();
     }
 
     public function contactMessages(): HasMany
